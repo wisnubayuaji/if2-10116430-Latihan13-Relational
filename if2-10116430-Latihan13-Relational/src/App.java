68bfcd5aaa478a1647b33cb5a453f9161ec767cd
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-20 19:45:51 +0700

    Add files via upload

---
+++ if2-10116430-Latihan13-Relational/src/App.java
@@ -0,0 +1,18 @@
+/**
+ * @author
+ * Nim  : 10116430
+ * Nama : Wisnu Bayu Aji
+ * Kelas: IF-2
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        int bi11 = 10;
+        int bi12 = 20;
+        System.out.println("a == b = " + (bi11 == bi12));
+        System.out.println("a != b = " + (bi11 != bi12));
+        System.out.println("a > b = " + (bi11 > bi12));
+        System.out.println("a < b = " + (bi11 < bi12));
+        System.out.println("a >= b = " + (bi11 >=bi12));
+        System.out.println("a <= b = " + (bi11 <= bi12));
+    }
+}
